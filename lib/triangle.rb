ee10302eb686f66cc18f759088046175d4d20d21
Chloe Gell <gellert.chloe@gmail.com>
2020-02-05 22:03:57 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,14 +7,14 @@ class Triangle
     @side3 = side3 
   end
   def kind 
-    if(@side1 <= 0 || @side2 <= 0 || @side3 <= 0)
+    if @side1 <= 0 || @side2 <= 0 || @side3 <= 0
       begin
         raise TriangleError 
       rescue TriangleError => error 
         puts error.message 
       end 
     end 
-    if(@side1 == @side2 && @side2 == @side3)
+    if (@side1 == @side2 && @side2 == @side3)
       return :equilateral 
     elsif(@side1 == @side2 || @side2 == @side3 || @side1 == @side3)
       return :isosceles
@@ -34,17 +34,4 @@ class Triangle
     end 
   end 
 end
-
-  def get_married(person)
-    self.partner = person
-    if person.class != Person
-      begin
-        raise PartnerError
-      rescue PartnerError => error
-          puts error.message
-      end
-    else
-      person.partner = self
-    end
-  end
 
