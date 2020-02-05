247ae8a545102231c1bb41de9f6c19da1f7de464
Chloe Gell <gellert.chloe@gmail.com>
2020-02-05 22:00:13 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,7 +7,7 @@ class Triangle
     @side3 = side3 
   end
   def kind 
-    if(@side1 <= 0 || @side2 <= 0 || @side3 <= 0)
+    if(@side1 <== 0 || @side2 <== 0 || @side3 <== 0)
       begin
         raise TriangleError 
       rescue TriangleError => error 
