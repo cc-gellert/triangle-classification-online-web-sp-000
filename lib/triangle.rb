02d04fcde2ee4d87c58c2d05735b2365e8accccc
Chloe Gell <gellert.chloe@gmail.com>
2020-02-05 22:08:17 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,6 +12,11 @@ class Triangle
         raise TriangleError 
       end 
     end 
+    if (@side1 + @side2) <= @side3 || (@side2 + @side3) <= @side1 || (@side1 + @side3) <= @side2 
+      begin 
+        raise TriangleError 
+      end 
+    end 
     if (@side1 == @side2 && @side2 == @side3)
       return :equilateral 
     elsif(@side1 == @side2 || @side2 == @side3 || @side1 == @side3)
